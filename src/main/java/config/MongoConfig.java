b6e17882e420357ede181dce5d268620e5a3e0ff
Michael Geljic <mg3178@rit.edu>
2025-12-09 13:39:25 +0100

    mongo

--- src/main/java/config/MongoConfig.java
+++ src/main/java/config/MongoConfig.java
@@ -8,7 +8,8 @@ public class MongoConfig {
 
     // Your Atlas URI with username and password included
     private static final String URI =
-            "mongodb+srv://USERNAME:PASSWORD@CHANGEME.mongodb.net/";
+            "mongodb+srv://mg3178:4321@playlistmanager-cluster.hovcfc8.mongodb.net/?appName=playlistmanager-cluster";
+
 
 
     private static final String DB_NAME = "musicdb";
